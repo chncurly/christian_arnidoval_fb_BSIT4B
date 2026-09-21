867e741d99abce7e12d833159d562adda8b1c25e
Christian Arnidoval <chanooh14@gmail.com>
2026-09-21 15:59:57 +0800

    new code

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,69 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'firebase_options.dart';
-Future<void> main() async {
- WidgetsFlutterBinding.ensureInitialized();
- await Firebase.initializeApp(
- options: DefaultFirebaseOptions.currentPlatform,
- );
- runApp(const MyApp());
+import 'package:flutter_firebase/student_page.dart';
 
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
- const MyApp({super.key});
- @override
- Widget build(BuildContext context) {
- return MaterialApp(
- debugShowCheckedModeBanner: false,
- title: 'Flutter Firebase App',
- theme: ThemeData(
- colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
- useMaterial3: true,
- ),
- home: const FirebaseHomePage(),
- );
- }
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Firebase App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: FirebaseHomePage(),
+    );
+  }
 }
+
 class FirebaseHomePage extends StatelessWidget {
- const FirebaseHomePage({super.key});
- @override
- Widget build(BuildContext context) {
- return Scaffold(
- appBar: AppBar(
- title: const Text('Flutter Firebase'),
- centerTitle: true,
- ),
- body: const Center(
- child: Column(
- mainAxisAlignment: MainAxisAlignment.center,
- children: [
- Icon(Icons.cloud_done, size: 80, color: Colors.green),
- SizedBox(height: 16),
- Text(
- 'Firebase connected successfully!',
- style: TextStyle(
- fontSize: 20,
- fontWeight: FontWeight.bold,
- ),
- ),
- ],
- ),
- ),
- );
- }
+  const FirebaseHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Flutter Firebase'), centerTitle: true),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.cloud_done, size: 80, color: Colors.green),
+            SizedBox(height: 16),
+            Text(
+              'Firebase connected successfully!',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => StudentPage()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                foregroundColor: Colors.white,
+              ),
+              child: Text("Create Student"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
---
+++ lib/student_page.dart
@@ -0,0 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class StudentPage extends StatefulWidget {
+  const StudentPage({super.key});
+
+  @override
+  State<StudentPage> createState() => _StudentPageState();
+}
+
+class _StudentPageState extends State<StudentPage> {
+  Future<void> saveTestData() async {
+    await FirebaseFirestore.instance.collection('students').add({
+      'name': 'Juan Dela Cruz',
+      'course': 'BS Computer Science',
+      'createdAt': FieldValue.serverTimestamp(),
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Student Page")),
+      body: Column(
+        children: [
+          FilledButton(
+            onPressed: saveTestData,
+            child: const Text("Save test Student"),
+          ),
+        ],
+      ),
+    );
+  }
+}
